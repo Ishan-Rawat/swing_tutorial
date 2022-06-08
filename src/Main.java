3958a8ce8f976506d3ea17d638ad556ebb2e2b21
Ishan-Rawat <rawatishan03@gmail.com>
2022-06-08 16:34:02 +0530

    changed layout manager and the layout of label

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import javax.swing.JFrame;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.color.*;
@@ -15,6 +16,8 @@ public class Main {
 		JPanel redPanel = new JPanel();
 		redPanel.setBackground(Color.red);
 		redPanel.setBounds(0, 0, 250, 250);
+		//panels by default use flow layout. But we can change it to whatever we want.
+		redPanel.setLayout(new BorderLayout());
 		
 		JPanel bluePanel = new JPanel();
 		bluePanel.setBackground(Color.blue);
@@ -24,11 +27,24 @@ public class Main {
 		greenPanel.setBackground(Color.green);
 		greenPanel.setBounds(0, 250, 500, 250);
 		
+		// Lets create a label which we can add to a panel
+		JLabel label = new JLabel();
+		label.setText("Hi!");
+		ImageIcon image = new ImageIcon("C:\\Users\\ishan\\Downloads\\hand2.png");
+		label.setIcon(image);
+		label.setVerticalTextPosition(JLabel.TOP);
+		label.setHorizontalTextPosition(JLabel.CENTER);
+		//after setting the layout manager of the panel to Border Layout we can again use methods like setVerticalPosition etc.
+		label.setVerticalAlignment(JLabel.TOP);
+		label.setHorizontalAlignment(JLabel.CENTER);
+		
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLayout(null);
 		frame.setSize(750, 750);
 		frame.setVisible(true);
+		
+		redPanel.add(label);
 		
 		frame.add(redPanel);
 		frame.add(greenPanel);
