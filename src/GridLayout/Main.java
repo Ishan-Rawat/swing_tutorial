e097b38978c8bb04248508dc0e7bdfeccb68a6ca
Ishan-Rawat <rawatishan03@gmail.com>
2022-06-21 01:04:48 +0530

    How to use grid layout

---
+++ src/GridLayout/Main.java
@@ -0,0 +1,32 @@
+package GridLayout;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Grid Layout:  places components in a grid of cells
+		// 				 Each component takes all the available space within its cell
+		//				 and each cell is the same size
+		
+		JFrame frame = new JFrame();
+		frame.setSize(500, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridLayout(3,3, 10, 10)); //arguments are number of rows, columns, horizontal margin, vertical margin
+		frame.add(new JButton("1"));
+		frame.add(new JButton("2"));
+		frame.add(new JButton("3"));
+		frame.add(new JButton("4"));
+		frame.add(new JButton("5"));
+		frame.add(new JButton("6"));
+		frame.add(new JButton("7"));
+		frame.add(new JButton("8"));
+		frame.add(new JButton("9"));
+		
+		frame.setVisible(true);
+	}
+
+}
